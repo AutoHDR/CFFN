c92b915dda7a2ee3972ae35ef63452bb1004df92
AUTOHDR <meng.wang@tju.edu.cn>
2022-12-19 14:54:15 +0800

    del code

--- coarse_stage/models.py
+++ coarse_stage/models.py
@@ -253,6 +253,24 @@ def normal_init(m, mean, std):
         m.weight.data.normal_(mean, std)
         m.bias.data.zero_()
 
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        conv_block = [  nn.ReflectionPad2d(1),
+                        nn.Conv2d(in_features, in_features, 3),
+                        nn.InstanceNorm2d(in_features),
+                        nn.ReLU(inplace=True),
+                        nn.ReflectionPad2d(1),
+                        nn.Conv2d(in_features, in_features, 3),
+                        nn.InstanceNorm2d(in_features)  ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+        
 class CycleGANGen(nn.Module):
     def __init__(self, input_nc, output_nc, n_residual_blocks=9):
         super(CycleGANGen, self).__init__()
